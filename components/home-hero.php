f92cbad3098e164d4ca089d90f1af3f338a4194a
Erin Flegg <erin@acornmade.com>
2021-05-19 12:18:30 -0700

    add home hero partial

---
+++ components/home-hero.php
@@ -0,0 +1,9 @@
+<div class="home-hero--container">
+    <div class="hero-img">
+        <img src="https://picsum.photos/1200" alt="">
+    </div>
+    <header class="hero-text">
+        <h1 class="text-center page--home__title">Awl Together Leather</h1>
+        <h3 class="text-center page--home__subtitle">Custom leatherwork & repairs</h3>
+    </header>
+</div>
--- page-home.php
+++ page-home.php
@@ -8,20 +8,16 @@ Template Name: Home Page
 
 <?php while (have_posts()): the_post();?>
 
-    <header class="page--home">
-        <h1 class="text-center page--home__title">Awl Together Leather</h1>
-        <h3 class="text-center page--home__subtitle">Repairing the Future of Cobbling</h3>
-    </header>
+   <?php include "components/home-hero.php" ;?>
 
     <div class="layout-container">
-
         <h2 class="w-50">We’re repairing the future of leatherworks, one sole at a time</h2>
-    <div class="listing-block--container layout-container row">
-        <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
-        <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
-        <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
-        <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
-    </div>
+        <div class="listing-block--container layout-container row">
+            <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
+            <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
+            <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
+            <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
+        </div>
     </div>
 
 
