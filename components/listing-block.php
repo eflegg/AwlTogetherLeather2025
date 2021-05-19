be1988b182b3d08da7d9a6852708c4a3db0b0063
Erin Flegg <erin@acornmade.com>
2021-05-19 12:05:19 -0700

    listing block layout container

--- components/listing-block.php
+++ components/listing-block.php
@@ -1,27 +1,14 @@
+<div class="block-container">
+    <div class="block-image">
+<img src="https://picsum.photos/400" alt="">
+    </div>
+    <div class="block-text">
+        <h3>Custom Goods</h3>
+        <p>Big or small, we love to make whatever your heart desires</p>
+    </div>
+</div>
 
 
 
-<?php
-$recipeHeader = get_field('recipe_header');
-if ($recipeHeader): ?>
-<div class="col-12 col-sm-6 col-md-4 container--recipes__single">
-<?php
-$image = $recipeHeader['image'];
-if ($image): ?>
-  <a href="<?php the_permalink();?>">
-        <?php include "image.php";?>
-        </a>
-    <?php endif;?>
-<?php
-$recipeTitle = $recipeHeader['recipe_title'];
-if ($recipeTitle): ?>
 
-            <!-- <img src="http://localhost:8888/whatsfordinner/wp-content/uploads/2020/03/147735EMfl120212-R1-005-2-scaled.jpg" alt=""> -->
-            <a href="<?php the_permalink();?>">
-            <h3 class="mt-3"><?php echo $recipeTitle; ?></h3>
-            </a>
-    <?php endif;?>
 
-        </div>
-
-        <?php endif;?>
--- page-home.php
+++ page-home.php
@@ -8,64 +8,30 @@ Template Name: Home Page
 
 <?php while (have_posts()): the_post();?>
 
-<header class="page--home">
-    <h1 class="text-center page--home__title">Awl Together Leather</h1>
-    <h3 class="text-center page--home__subtitle">Repairing the Future of Cobbling</h3>
-</header>
+    <header class="page--home">
+        <h1 class="text-center page--home__title">Awl Together Leather</h1>
+        <h3 class="text-center page--home__subtitle">Repairing the Future of Cobbling</h3>
+    </header>
+
+    <div class="layout-container">
+
+        <h2 class="w-50">We’re repairing the future of leatherworks, one sole at a time</h2>
+    <div class="listing-block--container layout-container row">
+        <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
+        <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
+        <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
+        <div class="col-12 col-md-6"><?php include "components/listing-block.php";?></div>
+    </div>
+    </div>
 
 
-<section class="layout-container">
-    <article class="d-flex flex-row container--recipes">
 
 
-        <?php $postType = 'recipe';?>
-                <?php if ($postType): ?>
-                    <?php
-    // this file is going to have to have various post types to render...
-    $args = (array(
-        'post_type' => $postType,
-        'posts_per_page' => -1,
-
-    )
-    );
-    $query = new WP_Query($args);?>
-
-	<?php if ($query->have_posts()): while ($query->have_posts()): $query->the_post();?>
-																																														                        <?php endwhile;endif;
-    wp_reset_postdata()?>
-																	                            	<?php endif;?>
 
 
-        <!-- <div class="col-12 col-sm-6 col-md-4 container--recipes__single">
-            <img src="http://localhost:8888/whatsfordinner/wp-content/uploads/2020/03/147735EMfl120212-R1-005-2-scaled.jpg" alt="">
-            <h3 class="mt-3">kimchi fried rice</h3>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 container--recipes__single">
-            <img src="http://localhost:8888/whatsfordinner/wp-content/uploads/2020/03/33760012.jpg" alt="">
-            <h3>kimchi fried rice</h3>
-        </div>
-        <div class=" col-12 col-sm-6 col-md-4 container--recipes__single">
-            <img src="http://localhost:8888/whatsfordinner/wp-content/uploads/2020/03/34030022.jpg" alt="">
-            <h3>kimchi fried rice</h3>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 container--recipes__single">
-            <img src="http://localhost:8888/whatsfordinner/wp-content/uploads/2020/03/33760005.jpg" alt="">
-            <h3>kimchi fried rice</h3>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 container--recipes__single">
-            <img src="http://localhost:8888/whatsfordinner/wp-content/uploads/2020/03/34030023-scaled.jpg" alt="">
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 container--recipes__single">
-            <img src="http://localhost:8888/whatsfordinner/wp-content/uploads/2020/03/EF090320253391-20200311-07-scaled.jpg" alt="">
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 container--recipes__single">
-            <img src="http://localhost:8888/whatsfordinner/wp-content/uploads/2020/03/EF090320253391-20200311-08-scaled.jpg" alt="">
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 container--recipes__single">
-            <img src="http://localhost:8888/whatsfordinner/wp-content/uploads/2020/03/EF090320253391-20200311-06-scaled.jpg" alt="">
-        </div> -->
-    </article>
-</section>
+
+     
+ 
 
 
 
