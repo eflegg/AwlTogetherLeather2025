19d85d182db4d1d462fdfcde801afc72e662b42c
Erin Flegg <erin@acornmade.com>
2021-05-21 19:04:34 -0700

    blog display, test posts, access posts

--- page-blog.php
+++ page-blog.php
@@ -18,24 +18,72 @@ Template Name: Blog Page
     <div class="access-grid--container row">
 
     <?php
+    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
     $args = array (
         'post_type' => 'post',
         'posts_per_page' => 6,
+        'paged' => $paged
     );
     $blogQuery = new WP_query($args);
 
     if ( $blogQuery->have_posts() ) : 
         ?>
+         <?php the_posts_pagination(); ?>
             <?php while( $blogQuery->have_posts() ) : $blogQuery->the_post() ?>
             <div class="acces-block--outer col-12 col-md-6">
-            <?php include 'components/blogAccess-listing.php'; ?>
+          
+            <?php
+		the_title(
+			sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+			'</a></h2>'
+		);
+		?>
+        	<div class="entry-content">
+
+            <?php the_excerpt(); ?>
+
+
+            <?php
+            wp_link_pages(
+                array(
+                    'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+                    'after'  => '</div>',
+                )
+            );
+            ?>
+
+        </div>
+
         </div>
             <?php endwhile ?>
+            <div class="pagination">
+    <?php 
+        echo paginate_links( array(
+            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+            'total'        => $blogQuery->max_num_pages,
+            'current'      => max( 1, get_query_var( 'paged' ) ),
+            'format'       => '?paged=%#%',
+            'show_all'     => false,
+            'type'         => 'plain',
+            'end_size'     => 2,
+            'mid_size'     => 1,
+            'prev_next'    => true,
+            'prev_text'    => sprintf( '<i></i> %1$s', __( 'Newer Posts', 'text-domain' ) ),
+            'next_text'    => sprintf( '%1$s <i></i>', __( 'Older Posts', 'text-domain' ) ),
+            'add_args'     => false,
+            'add_fragment' => '',
+        ) );
+    ?>
+</div>
+           
         <?php else : ?>
            <p>no posts</p>
         <?php endif ?>
        
     </div>
+<div class="pagination">
+<?php get_the_posts_pagination() ?>
+</div>
 
     <!-- test for outgoing messsage here. text + button -->
 </div>
