29e5856f79855ef5442a138c28a23997b91f4c26
Erin Flegg <erin@acornmade.com>
2021-05-26 14:52:56 -0700

    clean up

---
+++ page-careers.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Careers Page
+ */
+?>
+
+<?php get_header();?>
+
+<?php while (have_posts()): the_post();?>
+
+
+
+<!-- close php loop -->
+<?php endwhile;?>
+
+<?php get_footer();?>
