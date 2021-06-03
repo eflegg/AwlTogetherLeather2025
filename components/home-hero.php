e9e5bdd76f6a63b6a1876c79afcad997cb5ac674
Erin Flegg <erin@acornmade.com>
2021-06-03 11:15:27 -0700

    tweaks to open menu, homepage hero overlay

--- components/home-hero.php
+++ components/home-hero.php
@@ -2,6 +2,7 @@
     <div class="hero-img">
         <img src="https://picsum.photos/1200" alt="">
     </div>
+    <div class="overlay"></div>
     <header class="hero-text">
         <h1 class="text-center page--home__title">Awl Together Leather</h1>
         <h3 class="text-center page--home__subtitle">Custom leatherwork & repairs</h3>
