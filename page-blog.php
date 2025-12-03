66ef5958704ad8bcbf01bd21ec196d2167b78bc9
Erin Flegg <ehflegg@gmail.com>
2025-12-02 17:20:19 -0800

    added in resusable filter file, need to check it

--- page-blog.php
+++ page-blog.php
@@ -8,9 +8,39 @@ Template Name: Blog Page
 
 <?php while (have_posts()): the_post();?>
 
-<div class="layout-container">
-    Filter
-</div>
+
+
+<section class="section-container layout-container">
+
+
+
+
+    <?php 
+        $dataType = "post"; 
+        $category = "Blog Topics";
+        $path = "components/cards/blog-card.php";
+        ?>
+    <?php include 'components/reusable-filter.php';?>
+
+<ul class="card-container products">
+        
+    <?php
+        $args = array(
+            'post_type' => 'post',
+            'orderby' => 'menu_order',
+            'order' => 'ASC',
+            'posts_per_page' => -1,
+    );
+
+    $the_query = new WP_Query( $args ); ?>
+	     <?php if ( $the_query->have_posts() ) : ?>
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                <?php include 'components/cards/blog-card.php';?>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            <?php endif; ?>	 			    
+            </ul>
+</section>
 
 
 <div class="layout-container">
