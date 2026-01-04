dd956f83a0e240af861148a91542e24318a0f2d2
Erin Flegg <ehflegg@gmail.com>
2026-01-03 16:45:03 -0800

    fix pagination width

--- single-services.php
+++ single-services.php
@@ -1,55 +1,57 @@
-<?php get_header();?>
+<?php get_header(); ?>
 
 
+<div class="green-gradient">
+    <?php include "components/custom-page-header.php"; ?>
+</div>
 <div class="service-single--container">
-    <?php include "components/custom-page-header.php";?>
 
     <div class="service--inner ">
         <div class="service-description col-12 col-md-6">
             <?php
             $serviceDescrip = get_field('service_description');
-            if($serviceDescrip):?>
-            <p>
-            <?php echo $serviceDescrip;?>
-            </p>
-            <?php endif;?>
+            if ($serviceDescrip): ?>
+                <p>
+                    <?php echo $serviceDescrip; ?>
+                </p>
+            <?php endif; ?>
             <div class="btn--primary ">
-                <?php echo '<a href="' . home_url().'/book-appointment">Book Intake</a>';?>
+                <?php echo '<a href="' . home_url() . '/book-appointment">Book Intake</a>'; ?>
             </div>
         </div>
         <div class="service-image col-12 col-md-6">
-            <?php echo get_the_post_thumbnail();?>
+            <?php echo get_the_post_thumbnail(); ?>
         </div>
     </div>
     <div class="service-gallery">
-        <?php 
+        <?php
         $images = get_field('service_gallery');
         $size = 'full'; // (thumbnail, medium, large, full or custom size)
-        if( $images ): ?>
+        if ($images): ?>
             <ul class="gallery--inner row">
-                <?php foreach( $images as $image ): ?>
+                <?php foreach ($images as $image): ?>
                     <li class="gallery--single col-12 col-sm-6 col-md-3">
-                    <img src="<?php echo esc_url($image['sizes']['large']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                        <img src="<?php echo esc_url($image['sizes']['large']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
                     </li>
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
     </div>
 
-    <?php 
+    <?php
     // Get the current post type
-   $postType = get_post_type();?>
-  
+    $postType = get_post_type(); ?>
+
     <div class="btn--text d-flex btn--back">
-      
-            <i>&xrarr;</i>
-            
-            <?php echo '<a href="' . home_url().'/'.($postType) . '"><h3 >Back to Services</h3></a>';
- ?>
+
+        <i>&xrarr;</i>
+
+        <?php echo '<a href="' . home_url() . '/' . ($postType) . '"><h3 >Back to Services</h3></a>';
+        ?>
         </a>
     </div>
 
 
 </div>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
