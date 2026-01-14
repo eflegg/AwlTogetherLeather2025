64d26c681ca32dc9dc37bd8958a9cad22c12a3b6
Erin Flegg <ehflegg@gmail.com>
2026-01-14 14:46:22 -0800

    access updates, add aria label to services listing block links to say where they go, increase touch area for header buttons

--- components/service-listing-block.php
+++ components/service-listing-block.php
@@ -1,14 +1,15 @@
 <div class="service-block__container content fade-me">
-    <a class="service-block__image service-block__image--odd" href=<?php the_permalink(); ?>>
+    <a aria-label="Links to page about <?php the_title(); ?>" class="service-block__image service-block__image--odd" href=<?php the_permalink(); ?>>
         <figure>
             <?php echo get_the_post_thumbnail(); ?>
         </figure>
     </a>
 
     <div class="service-block__text">
-        <a href=<?php the_permalink(); ?>>
+        <?php
+        $serviceTitle = get_field('service_title'); ?>
+        <a href=<?php the_permalink(); ?> aria-label="Links to page about <?php echo $serviceTitle; ?>">
             <?php
-            $serviceTitle = get_field('service_title');
             if ($serviceTitle): ?>
                 <h2><?php echo $serviceTitle; ?></h2>
             <?php endif; ?>
@@ -23,9 +24,9 @@
                     <?php endif; ?>
         </a>
         <!-- button -->
-        <a href=<?php the_permalink(); ?> class="btn btn--listing btn--primary btn--square">Learn More</a>
+        <a href=<?php the_permalink(); ?> aria-label="Links to page about <?php echo $serviceTitle; ?>" class="btn btn--listing btn--primary btn--square">Learn More</a>
     </div>
-    <a class="service-block__image service-block__image--even" href=<?php the_permalink(); ?>>
+    <a class="service-block__image service-block__image--even" aria-label="Links to page about <?php echo $serviceTitle; ?>" href=<?php the_permalink(); ?>>
         <figure>
             <?php echo get_the_post_thumbnail(); ?>
         </figure>
