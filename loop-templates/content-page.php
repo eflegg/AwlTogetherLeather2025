623bc7e9d05efe9f403172f3a91d3150e7121781
Erin Flegg <ehflegg@gmail.com>
2026-02-11 10:39:50 -0800

    move layout container class to article element on page php so that featured image is included

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -10,12 +10,12 @@
 defined('ABSPATH') || exit;
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article class="layout-container" <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 
 	<?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
 
-	<div class="entry-content layout-container">
+	<div class="entry-content ">
 
 		<?php the_content(); ?>
 
